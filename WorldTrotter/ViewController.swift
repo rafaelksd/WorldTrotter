98d4aa314077dd009615a4eb6b7ad52620ea2bca
ravelar <rafaelksd@live.com>
2019-11-15 22:12:14 -0600

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Rafael Avelar on 15/11/19.
+//  Copyright © 2019 Rafael Avelar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
